fe5c8cc79a58cb68bad4c0e69e456f1e46333b88
supermasita <francisco@mascia.com.ar>
2012-04-30 20:09:49 -0300

    byebye incoming_path

--- lib/config.py
+++ lib/config.py
@@ -3,8 +3,10 @@
 
 # Core root
 core_root = "/var/www/html/ufe/"
+
 # Absolute path to folder where videos arrive 
-incoming = "/var/www/html/ufe/video_origin/"
+# incoming = "/var/www/html/ufe/video_origin/" (!)DEPRECATED
+
 # Absolute path to folder where original videos are copied
 original = "/var/www/html/ufe/video_original/"
 # Absolute path to folder where encoded videos are created
@@ -13,7 +15,7 @@ encoded = "/var/www/html/ufe/video_encoded/"
 tmppath = "/var/tmp"
 
 # Tolerance from last access to a video in video_origin
-# Used by "ufe-checker" to determine if video can be added for encoding
+# Used by "ufe-add" to determine if video can be added for encoding
 timedif_to = 10
 
 # This host's name in db table "servers"
