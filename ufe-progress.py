358309c8bdcfa9c9b4a07c601fa51039ed8b1ed9
supermasita <francisco@mascia.com.ar>
2012-06-06 02:49:23 -0300

    updated progress, now commits to DB

--- ufe-progress.py
+++ ufe-progress.py
@@ -45,34 +45,37 @@ def update_encode_percent(vhash,vpid,encode_percent):
 # Get video list
 running_videos_list = get_running_videos()
 
-for video in running_videos_list :
-	vhash = video[0]
-	vpid = video[1]
-	encode_file_name, encode_file_ext = os.path.splitext(video[2])
-	encode_file_log = "%s.log" % encode_file_name
-	# Let parse the log
-	log = open("%s/%s/%s" % (encoded, vhash, encode_file_log), "r")
-	#
-	for line in log:
-		# frame=   29 fps=  0 q=2.6 size=     114kB time=0.79 bitrate=1181.0kbits/s
+if len(running_videos_list)>0 :
+	for video in running_videos_list :
+		vhash = video[0]
+		vpid = video[1]
+		encode_file_name, encode_file_ext = os.path.splitext(video[2])
+		encode_file_log = "%s.log" % encode_file_name
+		# Let parse the log
+		log = open("%s/%s/%s" % (encoded, vhash, encode_file_log), "r")
 		#
-		duration_re = re.compile("""Duration: (?P<duration>\S+),""")
-		match = re.search(duration_re, line, re.M)
-		m = re.findall(duration_re, line)
-		if match is not None:
-			duration = m[0]
-		#
-		time_done_re = re.compile("""time=(?P<time>\S+)""")
-		match = re.search(time_done_re, line, re.M)
-		m = re.findall(time_done_re, line)
-		if match is not None:
-			time_done = m[-1:][0]
-			#print time_done
-	log.close()
+		for line in log:
+			# frame=   29 fps=  0 q=2.6 size=     114kB time=0.79 bitrate=1181.0kbits/s
+			#
+			duration_re = re.compile("""Duration: (?P<duration>\S+),""")
+			match = re.search(duration_re, line, re.M)
+			m = re.findall(duration_re, line)
+			if match is not None:
+				duration = m[0]
+			#
+			time_done_re = re.compile("""time=(?P<time>\S+)""")
+			match = re.search(time_done_re, line, re.M)
+			m = re.findall(time_done_re, line)
+			if match is not None:
+				time_done = m[-1:][0]
+				#print time_done
+		log.close()
 
-	time_done_int = float(time_done.replace(":", "").replace(".",""))
-	duration_int = float(duration.replace(":", "").replace(".",""))
-	encode_percent = round(float((time_done_int*100 )/duration_int),2)
-	#
-	update_encode_percent(vhash,vpid,encode_percent)
+		time_done_int = float(time_done.replace(":", "").replace(".",""))
+		duration_int = float(duration.replace(":", "").replace(".",""))
+		encode_percent = round(float((time_done_int*100 )/duration_int),2)
+		#
+		update_encode_percent(vhash,vpid,encode_percent)
+else :
+	print "No videos running"
 
