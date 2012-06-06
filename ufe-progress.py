319b42ee049358dcd64164e8ea190ba9d5cccb9d
supermasita <francisco@mascia.com.ar>
2012-06-06 02:37:26 -0300

    updated progress, now commits to DB

--- ufe-progress.py
+++ ufe-progress.py
@@ -1,30 +1,78 @@
 # coding: utf-8
 
-import re, time
+import re
+import os
+import time
+from lib.common import *
+from lib.config import *
 
-log = open("/var/www/html/ufe/video_encoded/6f67484ea6/la_duena-6f67484ea6-360pw.log", "r")
-# frame=   29 fps=  0 q=2.6 size=     114kB time=0.79 bitrate=1181.0kbits/s
 
-for line in log:
-	
-	duration_re = re.compile("""Duration: (?P<duration>\S+),""")
-        match = re.search(duration_re, line, re.M)
-        m = re.findall(duration_re, line)
-	if match is not None:
-                duration = m[0]
-                #print duration
+#
+##
+###
 
-	time_done_re = re.compile("""time=(?P<time>\S+)""")
-	match = re.search(time_done_re, line, re.M)
-	m = re.findall(time_done_re, line)
-	if match is not None:
-		time_done = m[-1:][0]
-		#print time_done
+def get_running_videos() :
+        """List of vhash, vpids and filename of videos that are being encoded.
+        """
+        db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_database )
+        cursor = db.cursor()
+        cursor.execute("SELECT vhash, vpid, encode_file from video_encoded where encode_status=2 and server_name='%s';" % server_name)
+	results = cursor.fetchall()
+	cursor.close()
+	db.close()	
+	running_videos_list = []
+	for registry in results :
+		vhash = registry[0]
+		vpid = registry[1]
+		encode_file = registry[2]
+		running_videos_list.append([vhash,vpid,encode_file])
+	return running_videos_list
 
-log.close()
+def update_encode_percent(vhash,vpid,encode_percent):
+	"""Updates encode_percent field of video_encoded table.
+	"""
+	db = MySQLdb.connect(host=db_host, user=db_user, passwd=db_pass, db=db_database )
+        cursor = db.cursor()
+        cursor.execute("UPDATE video_encoded set encode_percent='%f' where vhash='%s' and vpid=%i;" % (encode_percent, vhash, vpid))
+        cursor.close()
+	db.commit()
+        db.close()
+		
+#
+##
+###
 
-time_done_int = float(time_done.replace(":", "").replace(".",""))
-duration_int = float(duration.replace(":", "").replace(".",""))
-print time_done_int, duration_int
-print round(float((time_done_int*100 )/duration_int),2),"%"
+# Get video list
+running_videos_list = get_running_videos()
+
+for video in running_videos_list :
+	vhash = video[0]
+	vpid = video[1]
+	encode_file_name, encode_file_ext = os.path.splitext(video[2])
+	encode_file_log = "%s.log" % encode_file_name
+	# Let parse the log
+	log = open("%s/%s/%s" % (encoded, vhash, encode_file_log), "r")
+	#
+	for line in log:
+		# frame=   29 fps=  0 q=2.6 size=     114kB time=0.79 bitrate=1181.0kbits/s
+		#
+		duration_re = re.compile("""Duration: (?P<duration>\S+),""")
+		match = re.search(duration_re, line, re.M)
+		m = re.findall(duration_re, line)
+		if match is not None:
+			duration = m[0]
+		#
+		time_done_re = re.compile("""time=(?P<time>\S+)""")
+		match = re.search(time_done_re, line, re.M)
+		m = re.findall(time_done_re, line)
+		if match is not None:
+			time_done = m[-1:][0]
+			#print time_done
+	log.close()
+
+	time_done_int = float(time_done.replace(":", "").replace(".",""))
+	duration_int = float(duration.replace(":", "").replace(".",""))
+	encode_percent = round(float((time_done_int*100 )/duration_int),2)
+	#
+	update_encode_percent(vhash,vpid,encode_percent)
 
