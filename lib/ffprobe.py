45fcacbe603189f3df6b682f45acdde04c7bb5ef
supermasita <root@uft1208.(none)>
2015-12-10 02:32:19 +0000

    more ffprobe

--- lib/ffprobe.py
+++ lib/ffprobe.py
@@ -8,6 +8,7 @@ import os, sys
 import simplejson as json
 from pprint import pprint
 
+# isvideo, video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br, video_f, audio_f, file_format
 
 def parse(filename):
     command = ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", filename]
@@ -16,7 +17,14 @@ def parse(filename):
  
     data = json.loads(p.stdout.read())
     p.stdout.close()
-    print data["streams"][0]["codec_name"]
+    print data
+    print "codec_name %s" % data["streams"][0]["codec_name"]
+    print "height %i" % data["streams"][0]["height"]
+    print "width %i" % data["streams"][0]["width"]
+    print "display_aspect_ratio %s" % data["streams"][0]["display_aspect_ratio"]
+    print "bit_rate %s" % data["streams"][0]["bit_rate"]
+    print "duration %s" % data["streams"][0]["duration"]
+
 
 
 parse("../sampleñí.mp4")
