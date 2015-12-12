db5cf9f45b98c049e8cccaee2d937a2ce7801959
supermasita <root@uft1208.(none)>
2015-12-12 16:00:00 +0000

    mapped all the metadata needed

--- lib/ffprobe.py
+++ lib/ffprobe.py
@@ -6,7 +6,7 @@
 from subprocess import Popen, PIPE
 import os, sys
 import simplejson as json
-from pprint import pprint
+#from pprint import pprint
 
 # isvideo, video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br, video_f, audio_f, file_format
 
@@ -17,13 +17,18 @@ def parse(filename):
  
     data = json.loads(p.stdout.read())
     p.stdout.close()
-    print data
-    print "codec_name %s" % data["streams"][0]["codec_name"]
-    print "height %i" % data["streams"][0]["height"]
-    print "width %i" % data["streams"][0]["width"]
-    print "display_aspect_ratio %s" % data["streams"][0]["display_aspect_ratio"]
-    print "bit_rate %s" % data["streams"][0]["bit_rate"]
-    print "duration %s" % data["streams"][0]["duration"]
+    #print data
+    print "video_br %s" % data["streams"][0]["bit_rate"]
+    print "video_w %i" % data["streams"][0]["width"]
+    print "video_h %i" % data["streams"][0]["height"]
+    print "aspect_r %s" % data["streams"][0]["display_aspect_ratio"]
+    print "size %s" % data["format"]["size"]
+    print "duration %s" % data["format"]["duration"]
+    print "total_br %s" % data["format"]["bit_rate"]
+    print "audio_br %s" % data["streams"][1]["bit_rate"]
+    print "video_f %s" % data["streams"][0]["codec_name"]
+    print "audio_f %s" % data["streams"][1]["codec_name"]
+    print "file_format %s" % data["format"]["format_name"]
 
 
 
