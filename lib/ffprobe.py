2c7aa1fb55f8bf09725d31b5e8bae34d9dda9061
root <root@uft1212.(none)>
2015-12-15 00:42:38 +0000

    more on ffprobe

--- lib/ffprobe.py
+++ lib/ffprobe.py
@@ -2,42 +2,45 @@
 # -*- coding: utf-8 -*-
 
 # ffprobe -v quiet -print_format json -show_format -show_streams test.mp4
+# isvideo, video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br, video_f, audio_f, file_format
 
+from __future__ import division
 from subprocess import Popen, PIPE
 import os, sys
 import simplejson as json
-#from pprint import pprint
 import getopt
 
-# isvideo, video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br, video_f, audio_f, file_format
-argv = sys.argv[1:]
-opts, args = getopt.getopt(argv, "f:")
-
-for opt, arg in opts :
-    if opt == "-f" :
-	filename = arg
 
 def video_meta(stream_number,data):
-    print "codec_type %s" % data["streams"][stream_number]["codec_type"]
-    print "video_br %s" % data["streams"][stream_number]["bit_rate"]
-    print "video_w %i" % data["streams"][stream_number]["width"]
-    print "video_h %i" % data["streams"][stream_number]["height"]
-    print "aspect_r %s" % data["streams"][stream_number]["display_aspect_ratio"]
-    print "video_f %s" % data["streams"][stream_number]["codec_name"]
-    print "duration %s" % data["format"]["duration"]
-    print "size %s" % data["format"]["size"]
-    print "duration %s" % data["format"]["duration"]
-    print "total_br %s" % data["format"]["bit_rate"]
-    print "file_format %s" % data["format"]["format_name"]
+    codec_type = data["streams"][stream_number]["codec_type"]
+    video_br = data["streams"][stream_number]["bit_rate"]
+    video_w = data["streams"][stream_number]["width"]
+    video_h = data["streams"][stream_number]["height"]
+    #aspect_r = data["streams"][stream_number]["display_aspect_ratio"]
+    aspect_r = "%.2f" % (video_w/video_h)
+    video_f = data["streams"][stream_number]["codec_name"]
+    duration = data["format"]["duration"]
+    size = data["format"]["size"]
+    duration = data["format"]["duration"]
+    total_br = data["format"]["bit_rate"]
+    file_format = data["format"]["format_name"]
+
+    return codec_type, video_br, video_w, video_h, aspect_r, video_f, duration, size, duration, total_br, file_format
+
 
 def audio_meta(stream_number,data):
-    print "codec_type %s" % data["streams"][stream_number]["codec_type"]
-    print "audio_br %s" % data["streams"][stream_number]["bit_rate"]
-    print "audio_f %s" % data["streams"][stream_number]["codec_name"]
+    codec_type = data["streams"][stream_number]["codec_type"]
+    audio_br = data["streams"][stream_number]["bit_rate"]
+    audio_f = data["streams"][stream_number]["codec_name"]
+
+    return codec_type, audio_br, audio_f
 
 
-
-def parse(filename):
+def media_check(filename):
+    """Checks a file with Mediainfo, to know if it is a video.
+       Return isvideo, video_br, video_w, video_h, aspect_r, duration, size
+    """
+    #logthis('Checking with MediaInfo: %s' % (filename), stdout=0)
     command = ["ffprobe", "-v", "quiet", "-print_format", "json", "-show_format", "-show_streams", filename]
     p = Popen(command, stdout=PIPE)
     p.wait()
@@ -46,52 +49,45 @@ def parse(filename):
     p.stdout.close()
     
 
-    print "---------------------------"
-    print filename
-    print "---------------------------"
-    
 
     try:
         if data["streams"][0]["codec_type"] == "video":
-            video_meta(0,data)
+            codec_type, video_br, video_w, video_h, aspect_r, video_f, duration, size, duration, total_br, file_format = video_meta(0,data)
         elif data["streams"][0]["codec_type"] == "audio":
-            audio_meta(0,data)
+            codec_type, audio_br, audio_f = audio_meta(0,data)
     
         if data["streams"][1]["codec_type"] == "video":
-            video_meta(1,data)
+            codec_type, video_br, video_w, video_h, aspect_r, video_f, duration, size, duration, total_br, file_format = video_meta(1,data)
         elif data["streams"][1]["codec_type"] == "audio":
-            audio_meta(1,data)
+            codec_type, audio_br, audio_f = audio_meta(1,data)
+    
+        isvideo=True
+    
     except:
-        print "Not a video"
-
-    print "---------------------------"
- 
-    #try:
-    #    #print data
-    #    print "---------------------------"
-    #    print filename
-    #    print "---------------------------"
-    #    print "video_br %s" % data["streams"][0]["bit_rate"]
-    #    print "codec_type %s" % data["streams"][0]["codec_type"]
-    #    print "codec_type %s" % data["streams"][1]["codec_type"]
-    #    print "video_w %i" % data["streams"][0]["width"]
-    #    print "video_h %i" % data["streams"][0]["height"]
-    #    print "aspect_r %s" % data["streams"][0]["display_aspect_ratio"]
-    #    print "size %s" % data["format"]["size"]
-    #    print "duration %s" % data["format"]["duration"]
-    #    print "total_br %s" % data["format"]["bit_rate"]
-    #    print "audio_br %s" % data["streams"][1]["bit_rate"]
-    #    print "video_f %s" % data["streams"][0]["codec_name"]
-    #    print "audio_f %s" % data["streams"][1]["codec_name"]
-    #    print "file_format %s" % data["format"]["format_name"]
-    #    print "---------------------------"
-    #    print ""
-    #except:
-    #   print "is not a video"
-    #   print "---------------------------"
+        isvideo=False
+        video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br=0,0,0,0,0,0,0,0
+        video_f="none"
+        audio_f="none"
+        codec_type="none"
+        file_format="none"
+    
+    return isvideo, video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br, video_f, audio_f, file_format, audio_br, audio_f
 
 
+# Test
+argv = sys.argv[1:]
+opts, args = getopt.getopt(argv, "f:")
 
-#parse("../sampleñí.mp4")
-parse(filename)
+for opt, arg in opts :
+    if opt == "-f" :
+	filename = arg
 
+isvideo, video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br, video_f, audio_f, file_format, audio_br, audio_f = media_check(filename)
+
+print "---------------------------"
+print filename
+print "---------------------------"
+    
+print isvideo, video_br, video_w, video_h, aspect_r, duration, size, total_br, audio_br, video_f, audio_f, file_format, audio_br, audio_f
+
+print "---------------------------"
